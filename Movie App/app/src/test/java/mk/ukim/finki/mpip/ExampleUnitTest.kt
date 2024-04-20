ab5299c84502ed61613a1799576735f1fbb11388
StefanMarkoski <101571841+StefanMarkoski@users.noreply.github.com>
2024-04-20 15:36:17 +0200

    Add files via upload
    Simple Mobile Movie App made in Kotlin.

---
+++ Movie App/app/src/test/java/mk/ukim/finki/mpip/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package mk.ukim.finki.mpip
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
